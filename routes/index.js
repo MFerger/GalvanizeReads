cabf303962e4612275691851c10fbcff7f3b04e2
Michael Ferger <mferger92@gmail.com>
2016-04-07 14:53:36 -0600

    Have all the views created and mostly styled

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,25 @@ router.get('/', function(req, res, next) {
 router.get('/books', function(req, res, next) {
   res.render('books');
 });
+router.get('/authors', function(req, res, next) {
+  res.render('authors');
+});
+router.get('/authors/edit', function(req, res, next) {
+  res.render('authorEdit');
+});
+router.get('/authors/new', function(req, res, next) {
+  res.render('authorNew');
+});
+router.get('/books/edit', function(req, res, next) {
+  res.render('booksEdit');
+});
+router.get('/books/new', function(req, res, next) {
+  res.render('bookNew');
+});
+router.get('/authors/delete', function(req, res, next) {
+  res.render('authorDelete');
+});
+router.get('/books/delete', function(req, res, next) {
+  res.render('bookDelete');
+});
 module.exports = router;
